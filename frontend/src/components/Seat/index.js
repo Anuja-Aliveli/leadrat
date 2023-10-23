a6d647e500f6a96fa1ad2a4ddb234f376853efcb
Anuja-Aliveli <a.anuja54@gmail.com>
2023-10-23 11:44:35 +0000

    Layout design completed

--- frontend/src/components/Seat/index.js
+++ frontend/src/components/Seat/index.js
@@ -1,10 +1,47 @@
-import './index.css';
+import "./index.css";
 
-const Seat = props => {
-    const {tickets, ticketType, seatsArray} = props 
-    return (
-        <h1>Hello</h1>
-    )
-}
+const Seat = (props) => {
+  const { tickets, ticketType, seatsArray } = props;
+  const array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
+  const renderRow = (rowArray) => (
+    <div className="row-container">
+      <p className="letter">{rowArray[0].row}</p>
+      {rowArray.map((eachSeat) => (
+        <button
+          type="button"
+          key={eachSeat.id}
+          disabled={eachSeat.sold === 1}
+          className={`seat ${
+            eachSeat.visibility === 1
+              ? "hidden"
+              : eachSeat.sold === 1
+              ? "booked"
+              : ""
+          }`}
+        >
+          {eachSeat.id}
+        </button>
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="chart-container">
+      <h4 className="price-container">Premium-Rs. 470.00</h4>
+      {seatsArray.map((eachRow, index) => {
+          if(index === 3 || index === 7) {
+              const typeText = index === 3 ? 'Executive-Rs. 450.00' : 'Normal-Rs. 430.00';
+              return (
+                  <>
+                    <div key={index}>{renderRow(eachRow["row" + array[index]])}</div>
+                    <h4 className="price-container">{typeText}</h4>
+                  </>
+              )
+          }
+          return <div key={index}>{renderRow(eachRow["row" + array[index]])}</div>
+      })}
+    </div>
+  );
+};
 
 export default Seat;
